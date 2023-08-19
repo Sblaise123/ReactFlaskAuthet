382d21f8dfcac8a0ac3c6eef19df2a6d7d07c310
Sblaise123 <127683538+Sblaise123@users.noreply.github.com>
2023-08-19 01:40:01 +0000

    finished

---
+++ src/front/js/pages/login.js
@@ -0,0 +1,44 @@
+import React, { useState, useContext, useEffect } from "react";
+import { Context } from "../store/appContext";
+import { useNavigate } from "react-router-dom";
+import "../../styles/home.css";
+
+export const Login = () => {
+  const { store, actions } = useContext(Context);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const history = useNavigate();
+  const token = sessionStorage.getItem("token");
+  console.log("Your token: " + token);
+  const handleClick = () => {
+    actions.login(email, password);
+  };
+  useEffect(() => {
+    if (store.token && store.token != "" && store.token != undefined)
+      history("/");
+  });
+  return (
+    <div className="text-center mt-5">
+      <h1>Login</h1>
+      {store.token && store.token != "" && store.token != undefined ? (
+        "You are now logged in with token " + token
+      ) : (
+        <div>
+          <input
+            type="text"
+            placeholder="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button onClick={handleClick}>Login</button>
+        </div>
+      )}
+    </div>
+  );
+};
